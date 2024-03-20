b103bf5a083a074d47f57654bdf4870ace1f5800
ramses <ramses69@naver.com>
2024-03-20 21:01:02 +0900

    first

---
+++ jake/src/main/java/com/win/JakeApplication.java
@@ -0,0 +1,13 @@
+package com.win;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JakeApplication.class, args);
+	}
+
+}
